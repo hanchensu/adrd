59ea4b438e44eb6305cb43ddee0d758e90592576
Hanchen Su <hanchensu@sohu-inc.com>
2013-07-25 16:45:05 +0800

    asd

---
+++ py-sessionlog/test.py
@@ -0,0 +1,30 @@
+'''
+Created on 2013-6-2
+
+@author: Tekchen
+'''
+from sessionlog.ttypes import AdInfoOperation
+from thrift.protocol.TBinaryProtocol import TBinaryProtocol
+from thrift.transport.TTransport import TMemoryBuffer
+from thrift.TSerialization import serialize
+x = AdInfoOperation()
+x.adId='122'
+x.yyId='123'
+
+trans = TMemoryBuffer()
+prot = TBinaryProtocol(trans)
+x.write(prot)
+
+byteArray = trans.getvalue()
+
+print byteArray
+
+y = AdInfoOperation()
+
+trans2 = TMemoryBuffer(byteArray)
+prot2 = TBinaryProtocol(trans2)
+y.read(prot2)
+
+print y.adId+' '+y.yyId
+
+print serialize(x)
