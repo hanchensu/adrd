9df7b5623dd1a0f5daa25b63be1d8ae2e47d3aee
Hanchen Su <hanchensu@sohu-inc.com>
2013-07-25 17:20:32 +0800

    asd

---
+++ py-sessionlog/test2.py
@@ -0,0 +1,23 @@
+'''
+Created on 2013-6-2
+
+@author: Tekchen
+'''
+from sessionlog.ttypes import AdInfoOperation
+import base64
+
+from thrift.protocol import TBinaryProtocol
+from thrift.TSerialization import deserialize
+
+def main():
+    protocol_factory = TBinaryProtocol.TBinaryProtocolFactory
+    base64_string = 'CwABAAAABTEyMzQ1AA=='
+    print 'base64 string: '+base64_string # deserialize the string back into an object
+    decodeString = base64.urlsafe_b64decode(base64_string)
+    print decodeString
+    x = AdInfoOperation()
+    deserialize(x, decodeString, protocol_factory=protocol_factory())
+    print 'x.adId:'+x.adId
+
+if __name__ == '__main__':
+    main()
