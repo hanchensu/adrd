dbf7410b72d042935b8a867839de05fdbb695e71
Hanchen Su <hanchensu@sohu-inc.com>
2013-07-25 17:09:53 +0800

    123

---
+++ thrfit-test/src/service/demo/HelloImpl.java
@@ -0,0 +1,15 @@
+package service.demo;
+
+import org.apache.thrift.TException;
+
+import service.demo.Hello.Iface;
+
+public class HelloImpl implements Iface {
+
+	@Override
+	public String helloString(String word) throws TException {
+		System.out.println("get " + word);
+		return "hello " + word;
+	}
+
+}
