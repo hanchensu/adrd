bec4077f03630e7a64f67bf2e5829f0138b4c212
Hanchen Su <hanchensu@sohu-inc.com>
2013-07-25 17:03:25 +0800

    asd

---
+++ thrfit-test/src/service/demo/Client.java
@@ -0,0 +1,32 @@
+package service.demo;
+
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
+
+public class Client {
+
+	public void startClient() {
+		TTransport transport;
+		try {
+			transport = new TSocket("localhost", 1234);
+			TProtocol protocol = new TBinaryProtocol(transport);
+			Hello.Client client = new Hello.Client(protocol);
+			transport.open();
+			System.out.println(client.helloString("sda"));
+			transport.close();
+		} catch (TTransportException e) {
+			e.printStackTrace();
+		} catch (TException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		Client client = new Client();
+		client.startClient();
+	}
+}
