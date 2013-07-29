e93f7093e02bc9fbce18cb90dc2d71a9fabcdfd8
Hanchen Su <hanchensu@sohu-inc.com>
2013-07-29 13:33:24 +0800

    1333

--- sessionlog/src/main/java/com/sohu/adrd/data/sessionlog/plugin/util/StrlenVerifier.java
+++ sessionlog/src/main/java/com/sohu/adrd/data/sessionlog/plugin/util/StrlenVerifier.java
@@ -2,16 +2,23 @@ package com.sohu.adrd.data.sessionlog.plugin.util;
 
 
 public class StrlenVerifier implements Verifier{
-	private int length;
+	private int minLen;
+	private int maxLen;
 	
-	public StrlenVerifier(int length) {
-		this.length = length;
+	public StrlenVerifier(int min,int max) {
+		this.minLen = min;
+		this.maxLen = max;
+	}
+	
+	public StrlenVerifier(int max) {
+		this.minLen = 0;
+		this.maxLen = max;
 	}
 	
 	@Override
 	public boolean isValid(Object... objects) {
 		String value = (String) objects[0];
-		if (value.length() <= length) {
+		if (value.length() >= minLen && value.length() <= maxLen) {
 			return true;
 		}
 		return false;
