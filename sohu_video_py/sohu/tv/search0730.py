31bf003bfa8bc1d8dba763a41a612286155096b1
Hanchen Su <hanchensu@sohu-inc.com>
2013-08-12 18:39:41 +0800

    1839

--- sohu_video_py/sohu/tv/search0730.py
+++ sohu_video_py/sohu/tv/search0730.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 '''
 Created on Jul 1, 2013
 
@@ -60,60 +61,110 @@ for filename in searchFiles:
         
         searchMap[line.split('\t')[0],filename] = string.atoi(line.split('\t')[1])
 
-
+coeff_lst=[]
 #make search_cnt
-index = 0
-
-before_all = []
+for gap in range(10,20):
+    index = 0
+    train_xy = []
+    for x in album:
+        index = index + 1
+        searches = []
+        datePlayBegin = x[2]
+        dateSearchBegin = deltaDay(datePlayBegin, -searchBefore)
+        
+        for i in range(0, searchDuration):
+            searches.append(searchMap.get(tuple([x[1],deltaDay(dateSearchBegin,i)])))
+        
+        #log or not   
+#         for i in range(0, len(searches)):
+#             if searches[i] != None:
+#                 searches[i] = math.log(searches[i])
+                
+    #     print x[1]+'___'+x[2]+':'
+    #     print searches
+        
+    #     #处理none
+    #     for i in range(0,len(searches)):
+    #         if searches[i] == None:
+    #             if i == 0:
+    #                 searches[i] = searches[i+1]
+    #             elif i == (len(searches) - 1):
+    #                 searches[i] = searches[i-1]
+    #             else:
+    #                 searches[i] = (searches[i-1] + searches[i+1])/2
+          
+        
+    #     before_all.append([avg(searches[0:5]),avg(searches[5:])]);
+        train_xy.append([avg(searches[0:gap]),avg(searches[0:gap+1])]);
+    #     #求log
+    #     logsearches = []  
+    #     for i in searches:
+    #         logsearches.append(math.log(i)) 
+    #     print logsearches
+    #     
+    #     #画第index剧的情况
+    #     if index == 11:
+    #         print x[1]+'___'+x[2]+':'
+    #         print searches
+    #         plot.draw(range(1,len(searches)+1), searches,10)
+    
+    
+    for index in range(0,len(train_xy)):
+        train_xy[index].append(index+1)
+     
+    xx=[tuple([x[0],x[2]]) for x in train_xy]
+    yy=[tuple([x[1],x[2]]) for x in train_xy]
+     
+#     print sorted(xx,key = lambda asd:asd[0],reverse = True)
+#     print sorted(yy,key = lambda asd:asd[0],reverse = True)
+     
+     
+    xa=[x[0] for x in train_xy]
+    ya=[x[1] for x in train_xy]
+     
+    coeff_lst.append(plot.draw(xa, ya,1))
+    
+# for x in coeff_lst:
+#     print x
+    
+    
+    
+    
+sum = 0
 for x in album:
     index = index + 1
     searches = []
     datePlayBegin = x[2]
     dateSearchBegin = deltaDay(datePlayBegin, -searchBefore)
-    
     for i in range(0, searchDuration):
         searches.append(searchMap.get(tuple([x[1],deltaDay(dateSearchBegin,i)])))
-#     print x[1]+'___'+x[2]+':'
-#     print searches
     
-     
-#     for i in range(0,len(searches)):
-#         if searches[i] == None:
-#             if i == 0:
-#                 searches[i] = searches[i+1]
-#             elif i == (len(searches) - 1):
-#                 searches[i] = searches[i-1]
-#             else:
-#                 searches[i] = (searches[i-1] + searches[i+1])/2
-      
-#     print avg(searches[0:5]),avg(searches[5:])
+    #log or not
+#     for i in range(0, len(searches)):
+#         if searches[i] != None:
+#             searches[i] = math.log(searches[i])
     
-#     before_all.append([avg(searches[0:5]),avg(searches[5:])]);
-    before_all.append([avg(searches[0:10]),max(searches[5:])]);
+    init_value = avg(searches[0:10])
+    forecast_value = init_value;
+    steps = 5;
+    for step in range(0,steps):
+        forecast_value = coeff_lst[step][0] + coeff_lst[step][1]*forecast_value;
     
-#     logsearches = []  
-#     for i in searches:
-#         logsearches.append(math.log(i)) 
-#     print logsearches
-#      
-#     if index == 11:
-#         print x[1]+'___'+x[2]+':'
-#         print searches
-#         plot.draw(range(1,len(searches)+1), searches,10)
+    #log or not    
+#     init = math.pow(math.e,init_value)
+#     real = math.pow(math.e,avg(searches[0:10+steps]))
+#     forecast = math.pow(math.e,forecast_value)
+    
+    init = init_value
+    real = avg(searches[0:10+steps])
+    forecast = forecast_value
+    
+    print init, real, forecast, (forecast-real)*1.0/min(forecast,real)
+    
+    delta = (forecast-real)*1.0/min(forecast,real)
+    if delta < 0:
+        delta*=-1
+    sum+=delta
 
-for index in range(0,len(before_all)):
-    before_all[index].append(index+1)
- 
-xx=[tuple([x[0],x[2]]) for x in before_all]
-yy=[tuple([x[1],x[2]]) for x in before_all]
- 
-print before_all
-print sorted(xx,key = lambda asd:asd[0],reverse = True)
-print sorted(yy,key = lambda asd:asd[0],reverse = True)
- 
- 
-xa=[x[0] for x in before_all]
-ya=[x[1] for x in before_all]
- 
- 
-plot.draw(xa, ya,1)
+print sum / len(album)
+    
