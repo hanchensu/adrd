d716acec682929ec8bfe47731239e23d45265a81
hanchensu <hanchensu@sohu-inc.com>
2013-09-21 23:11:40 +0800

    2311

--- impala-fe/src/com/cloudera/impala/analysis/AnalysisContext.java
+++ impala-fe/src/com/cloudera/impala/analysis/AnalysisContext.java
@@ -242,7 +242,7 @@ public class AnalysisContext {
   }
   
   public static void main(String[] args) throws Exception {
-		String stmt = "select 5*(c + d) as fuck,max(b) from shc where a > 10 AND b < 10 group by a";
+		String stmt = "select 5*(c + d) as fuck,max(b) from shc a where a > 10 AND b < 10 group by a";
 		SqlScanner input = new SqlScanner(new StringReader(stmt));
 	    SqlParser parser = new SqlParser(input);
 	   
@@ -250,7 +250,8 @@ public class AnalysisContext {
       result.stmt = (StatementBase) parser.parse().value;
       
 	  result.analyzer = new Analyzer(new Catalog(), "default", new User("shc"));
-	  System.out.println(result.analyzer.getUser().getName());
+	  result.stmt.analyze(result.analyzer);
+	  System.out.println(result.analyzer);
     
 //      System.out.println(((SlotRef)((SelectStmt)result.stmt).getSelectList().getItems().get(0).getExpr().getChild(1).getChild(1)).getColumnName());
 	}
